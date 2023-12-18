20b38e6c229d0db8cf52d79478e222d6dedca29c
Andrea Pupa <229867@studenti.unimore.it>
2023-12-18 09:15:33 +0100

    Fix Maze size

--- scripts/scale_model.py
+++ scripts/scale_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import re
 
-filepath = "../models/smaze2d/"
+filepath = "../models/smaze_wall/"
 
 scale_factor = 0.5
 f = open(filepath+"model.sdf", "r")
@@ -9,13 +9,15 @@ f2 = open(filepath+"model2.sdf","w")
 lines = f.readlines()
 for line in lines:
     new_line = line
-    if "pose" in line:
-        n = re.findall("(\d+(?:\.\d+)?)",line)
-        np = [float(x)*scale_factor for x in n[0:3]]
-        nr = [float(x) for x in n[3:6]]
-        n = np+nr
-        new_line = "\t\t\t\t<pose>" + str(n[0:6]) + "</pose>\n"
-        replace_ch = ["[","]",","]
-        for c in replace_ch:
-            new_line = new_line.replace(c,"")
+    tags = ["pose", "size"]
+    for t in tags:
+        if t in line:
+            n = re.findall("(\d+(?:\.\d+)?)",line)
+            np = [float(x)*scale_factor for x in n[0:3]]
+            nr = [float(x) for x in n[3:6]]
+            n = np+nr
+            new_line = "\t\t\t\t<"+t +">"+ str(n[0:6]) + "</"+t+">\n"
+            replace_ch = ["[","]",","]
+            for c in replace_ch:
+                new_line = new_line.replace(c,"")
     f2.write(new_line)
